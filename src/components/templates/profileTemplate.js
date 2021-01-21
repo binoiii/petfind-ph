c48878d87dff29aa466c471483d529950bde5df1
binothird <albinoiii.cordova@gmail.com>
2021-01-22 03:27:12 +0800

    updated seo

--- src/components/templates/profileTemplate.js
+++ src/components/templates/profileTemplate.js
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../layout"
+import SEO from "../seo"
 import WelcomeBanner from "./welcomeBanner"
 import PetImage from "./petImage"
 import OwnerDetails from "./ownerDetails"
