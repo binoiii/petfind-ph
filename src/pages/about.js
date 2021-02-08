9bb9dbc60eacad1136e023022275b09c98d58b95
binothird <albinoiii.cordova@gmail.com>
2021-02-08 12:19:52 +0800

    updated

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import AboutUs from "../components/about/AboutUs"
+import AboutUs from "../components/about/aboutUs"
 import BackTop from "../components/utilities/backToTop"
 
 const About = () => (
